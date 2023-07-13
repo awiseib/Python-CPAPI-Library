8c591d9713ed50b7e8b7f31e27a19fc86d02668a
Andrew W <awise@interactivebrokers.com>
2023-07-13 12:15:21 -0500

    PNL Websocket

---
+++ Websockets/websocket_pnl.py
@@ -0,0 +1,27 @@
+import websocket
+import time
+import ssl
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("## CLOSED! ##")
+
+def on_open(ws):
+    print("Opened Connection")
+    time.sleep(3)
+    ws.send('spl+{}')
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp(
+        url="wss://localhost:5000/v1/api/ws",
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
+    )
+    ws.run_forever(sslopt={"cert_reqs":ssl.CERT_NONE})
