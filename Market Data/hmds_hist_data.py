d780279845f85eeece1eac930715e005879a0a7d
Andrew W <awise@interactivebrokers.com>
2024-07-01 11:52:57 -0500

    Improved oauth notation and request consistency.

--- Market Data/hmds_hist_data.py
+++ Market Data/hmds_hist_data.py
@@ -8,12 +8,17 @@ import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def hmds_data():
-    params_url = "https://localhost:5000/v1/api/hmds/history?conid=265598&period=1d&bar=1hour&outsideRth=false&barType=last"
-    params_req = requests.get(url=params_url, verify=False)
-    
-    params_json = json.dumps(params_req.json(), indent=2)
-    
-    print(params_json)
+    params_url = "https://localhost:5001/v1/api/hmds/history"
+    params = { "conid": 272093, "period": "4y", "bar": "1min" } 
+    params_req = requests.get(url=params_url, params=params, verify=False)
+    if params_req.status_code == 200:
+        print(params_req.status_code)
+        params_json = json.dumps(params_req.json(), indent=2)
+        print(params_json)
+    else:
+        print(params_req.status_code)
+        print(params_req.content)
+        
     
 if __name__ == "__main__":
     hmds_data()
--- Orders/bracketOrder.py
+++ Orders/bracketOrder.py
@@ -9,47 +9,50 @@ def orderRequest():
   
     base_url = "https://localhost:5001/v1/api/"
     endpoint = "iserver/account/DU5240685/orders"
+    
+    coid = "NSE_3"
+    conid = 687901547
 
     json_body = {
         "orders":[
             {
-            "cOID":"AAPL_BKT2",
-            "conid": 265598,
-            "orderType": "LMT",
-            "price":160.25,
+            "cOID": coid,
+            "conid": conid,
+            "orderType": "MKT",
+            # "price":160.25,
             "side": "BUY",
             "tif": "DAY",
-            "quantity": 1,
-            "outsideRth":1
+            "quantity": 25,
             },
             {
-            "parentId":"AAPL_BKT2",
-            "conid": 265598,
-            "orderType": "MKT",
-            # "price":160,
+            "parentId":coid,
+            "conid": conid,
+            "orderType": "LMT",
+            "price":70,
             "side": "SELL",
-            "tif": "DAY",
-            "quantity": 1,
-            "outsideRth":1
+            "tif": "GTC",
+            "quantity": 25
             },
             {
-            "parentId":"AAPL_BKT2",
-            "conid": 265598,
-            "orderType": "STP",
-            "price":163,
+            "parentId":coid,
+            "conid": conid,
+            "orderType": "TRAIL",
+            "trailingType":"amt",
+            "trailingAmt": 10,
             "side": "SELL",
             "tif": "DAY",
-            "quantity": 1,
-            "outsideRth":1
+            "quantity": 25
             }
         ]
     }
     
     order_req = requests.post(url=base_url+endpoint, verify=False, json=json_body)
-    order_json = json.dumps(order_req.json(), indent=2)
-
-    print(order_req.status_code)
-    print(order_json)
+    if order_req.status_code == 200:
+        order_json = json.dumps(order_req.json(), indent=2)
+        print(order_req.status_code)
+        print(order_json)
+    else:
+        print(order_req.status_code)
 
 if __name__ == "__main__":
     orderRequest()
